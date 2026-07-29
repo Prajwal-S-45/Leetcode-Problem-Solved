8e9f96513d13226053e7e686e25f140e2c69bfbc
Prajwal Angadi <165166755+Prajwal-S-45@users.noreply.github.com>
2026-07-29 23:35:28 +0530

    Time: 2 ms (99.31%), Space: 47.1 MB (40.53%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+class Solution {
+    // O(n)
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++)
+        {
+            int complement = target - nums[i];
+
+            if(map.containsKey(complement))
+                return new int[] {map.get(complement),i};
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+
+    }
+}
+
+// class Solution{
+//      Bruteforce -- O(n^2) 
+//     public int[] twoSum(int[] nums, int target) {
+//         for (int i = 0; i < nums.length; i++) {
+//             for (int j = i + 1; j < nums.length; j++) {
+//                 if (nums[i] + nums[j] == target) {
+//                     return new int[]{i, j};
+//                 }
+//             }
+//         }
+//         return new int[]{};
+//     }
+// }
