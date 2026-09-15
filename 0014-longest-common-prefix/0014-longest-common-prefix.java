5fe841759f13ca25ac1a7cc61afcf2e41cc488ce
Prajwal Angadi <165166755+Prajwal-S-45@users.noreply.github.com>
2026-09-15 23:14:33 +0530

    Time: 1 ms (59.91%), Space: 43.3 MB (32.2%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         if(strs == null || strs.length == 0)
@@ -14,5 +15,24 @@ class Solution {
             }
         }
         return prefix;
+    }
+}
+*/
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+
+        StringBuilder result = new StringBuilder();
+
+        char[] first = strs[0].toCharArray();
+        char[] last = strs[strs.length - 1].toCharArray();
+
+        for(int i = 0;i < first.length; i++){
+            if(first[i] != last[i])
+                break;
+            result.append(first[i]);
+        }
+        return result.toString();
     }
 }
