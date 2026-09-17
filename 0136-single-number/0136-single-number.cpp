8e379823f8b052e18c07f49b9ed82f0cc8168d74
Prajwal Angadi <165166755+Prajwal-S-45@users.noreply.github.com>
2026-09-17 23:54:26 +0530

    Time: 0 ms (100%), Space: 20.7 MB (24.21%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for(int i=0;i<size(nums);i++){
+            result ^= nums[i];
+        }
+        return result;
+    }
+};
